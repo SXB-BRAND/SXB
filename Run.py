b5d9d9421b49a9e7b96b52fdfeb5a252ca5465cf
SXB-BRAND <121631709+SXB-BRAND@users.noreply.github.com>
2023-01-04 09:53:56 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,3 +1,15 @@
-import SXB
-os.system('xdg-open https://facebook.com/groups/1582618178844097/')
-print('New Update Please Waiting...')
+import os,time,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+green = ('\033[1;32m')
+white = ('\033[1;37m')
+red = ('\033[1;31m')
+
+print('<------------------------------------>')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print(f'{red}[•] Join Over Facebook Group {white}')
+    os.system('xdg-open https://facebook.com/groups/1582618178844097//')
+    import SXB
+    time.sleep(2.05)
