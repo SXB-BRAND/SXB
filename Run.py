41a98e1493319772141495329aedb9c5e4cca58c
SXB-BRAND <121631709+SXB-BRAND@users.noreply.github.com>
2023-01-04 09:36:48 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,4 +1,4 @@
 import SXB
-os.system(xdg-open https://facebook.com/groups/1582618178844097/')
+os.system('xdg-open https://facebook.com/groups/1582618178844097/')
 git pull
 print('New Update Please Waiting...')
