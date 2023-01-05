e89b7e95b0cbc3ee8a93d999de71f99571fffd56
SXB-BRAND <121631709+SXB-BRAND@users.noreply.github.com>
2023-01-05 14:57:50 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,12 +1,12 @@
 import os,time,platform
 os.system('clear')
-print('[•] Checking Updates...')
+print('[•] Checking New Updates...')
 os.system('git pull')
 green = ('\033[1;32m')
 white = ('\033[1;37m')
 red = ('\033[1;31m')
 
-print('<------------------------------------>')
+
 bit = platform.architecture()[0]
 if bit=='64bit':
     print(f'{red}[•] Join Over Facebook Group {white}')
