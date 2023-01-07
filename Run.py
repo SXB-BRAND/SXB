5782beb09df95b88a49b8293c08ea184f39a1058
SXB-BRAND <121631709+SXB-BRAND@users.noreply.github.com>
2023-01-07 15:55:04 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -9,7 +9,7 @@ red = ('\033[1;31m')
 
 bit = platform.architecture()[0]
 if bit=='64bit':
-    print(f'{red}[•] Join Over Facebook Group {white}')
+    print(f'{green}[•] Join Over Facebook Group {white}')
     os.system('xdg-open https://facebook.com/groups/1582618178844097//')
     import pro
     time.sleep(2.05)
